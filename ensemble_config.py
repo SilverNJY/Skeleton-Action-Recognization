da1da660afe3f1b7b967a6bd7bc028658999be25
SilverNJY <129756904+SilverNJY@users.noreply.github.com>
2024-11-12 09:48:27 +0800

    Update ensemble_config.py

--- ensemble_config.py
+++ ensemble_config.py
@@ -1,22 +1,25 @@
-# config.py
-
 # 模型配置，包括每个模型的模态、模态权重和模型间权重
 model_config = {
     "ctrgcn": {
-        "modalities": ["k8", "k1", "k2", "k8_vel", "k1_vel", "k2_vel"],
-        "weights": [0.7, 0.7, 0.6, 0.3, 0.3, 0.3],
+        "modalities": ["k8", "k1f", "k2f", "k8_velf", "k2_vel", "k1_vel"],
+        "weights": [0.6, 0.8, 0.6, 0.6, 0.2, 0.],
         "weight": 0.8,
     },
     "infogcn": {
-        "modalities": ["k8", "k1", "k2", "k8_vel", "k1_vel", "k2_vel"],
-        "weights": [0.5, 0.7, 0.6, 0.2, 0.3, 0.2],
-        "weight": 0.7,
+        "modalities": ["k8f", "k1f", "k2", "k8_vel", "k1_vel"],
+        "weights": [0.4, 0.5, 0., 0.2, 0],
+        "weight": 0.5,
     },
     "mixformer": {
-        "modalities": ["k8", "k1", "k8_vel", "k1_vel"],
-        "weights": [0.5, 0.7, 0.3, 0],
-        "weight": 0.8,
-    }
+        "modalities": ["k8", "k1f", "k21", "k8_vel", "k2_velf"],
+        "weights": [0.4, 0.5, 0.3, 0.3, 0.5],
+        "weight": 0.5,
+    },
+    "jbf": {
+        "modalities": ["ctrgc", "ctrgc-vel"],
+        "weights": [0.5, 0.5],
+        "weight": 0.5,
+    },
 }
 
 # 数据集配置
